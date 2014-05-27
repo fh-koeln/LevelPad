f56462f35af06630ac84e48b4d695113127abce5
Volker <schaefer.volker@googlemail.com>
2014-05-27 16:33:55 +0200

    updated some models

--- app/models/Artifact.js
+++ app/models/Artifact.js
@@ -6,15 +6,17 @@ var mongoose = require('mongoose'),
 	Schema = mongoose.Schema,
 	ObjectId = Schema.ObjectId;
 
-var artifactSchema = new Schema({
-	subject: ObjectId, // Subject model
-	title: {type: String, default: 'no title' },
-	createdAt: {type: Date, default: Date.now},
-	description: {type: String, default: '' },
+var levelSchema = new Schema({
+	description: { type: String, required: true },
+	level: {type: Number, enum: [1, 2, 3, 4, 5] }
 });
 
-artifactSchema.statics.findBySubjectAndId = function (subject, artifact, callback) {
-	this.findOne({ subject: subject, _id: artifact }, callback);
-};
+
+var artifactSchema = new Schema({
+	title: {type: String, default: 'no title', required: true },
+	description: {type: String, default: '' },
+	levels: [levelSchema]
+	
+});
 
 module.exports = mongoose.model('Artifact', artifactSchema);
--- app/models/Module.js
+++ app/models/Module.js
@@ -6,8 +6,8 @@ var mongoose = require('mongoose'),
 	Schema = mongoose.Schema;
 
 var moduleSchema = new Schema({
-	shortName: { type: String },
-	name: { type: String }
+	shortName: { type: String, required: true },
+	name: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Module', moduleSchema);
