c9a8a2ef31a8ef5a12c4e8f02d2625a0699ac150
Christoph Jerolimov <jerolimov+git+2014@gmail.com>
2014-05-27 11:45:07 +0200

    Improve login mechanism.

--- app/public/controllers/NavigationController.js
+++ app/public/controllers/NavigationController.js
@@ -14,9 +14,18 @@ angular.module('levelPad').controller('NavigationController', ['$scope', '$route
 
     $scope.$location = $location;
 
+	/**
+	 * Manages the login / signup dialogs
+	 */
+
 	$scope.showLoginDialog = function() {
 		$log.info('Show modal login dialog...');
 		$('#loginDialog').modal();
+	};
+
+	$scope.hideLoginDialog = function() {
+		$log.info('Hide modal login dialog...');
+		$('#loginDialog').modal('hide');
 	};
 
 	$scope.showSignupDialog = function() {
@@ -24,19 +33,29 @@ angular.module('levelPad').controller('NavigationController', ['$scope', '$route
 		$('#signupDialog').modal();
 	};
 
-	authService.$watch('loggedIn', function(loggedIn) {
-		if (loggedIn) {
-			$log.info('Hide all modal dialogs...');
-			$('#loginDialog, #signupDialog').modal('hide');
+	$scope.hideSignupDialog = function() {
+		$log.info('Hide modal signup dialog...');
+		$('#signupDialog').modal('hide');
+	};
+
+	authService.$watch(function(user) {
+		console.log('logged in: ' + authService.loggedIn + ' -- user: ' + authService.user)
+		if (authService.loggedIn && authService.user) {
+			// Is logged in
+			$scope.hideLoginDialog();
+			$scope.hideSignupDialog();
 			if ($location.path() === '/' || $location.path() === '/login' || $location.path() === '/signup' || $location.path() === '/logout') {
 				$location.path('/');
 			}
-		} else {
-			if ($location.path() === '/logout') {
-				$location.path('/');
-			}
+		} else if (authService.loggedIn && !authService.user) {
+			// Should be logged in
+			$scope.showLoginDialog();
 		}
 	});
+
+	/**
+	 * Handles the navigation buttons and open the login / signup dialogs.
+	 */
 
     $scope.login = function() {
 		if ($location.path() === '/login' || $location.path() === '/signup') {
@@ -58,7 +77,10 @@ angular.module('levelPad').controller('NavigationController', ['$scope', '$route
         authService.logout();
     };
 
-	// Automatically load the current user status to update the navigation bar fields.
+	/**
+	 * Automatically load the current user status to update the navigation bar fields.
+	 */
+
 	authService.verifyStatus();
 
 }]);
