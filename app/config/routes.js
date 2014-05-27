1153019b9fafca4f521ca060c348b58d11192f2a
Dominik Schilling <dominikschilling+git@gmail.com>
2014-05-27 23:33:34 +0200

    Enable API authentification check

--- app/config/routes.js
+++ app/config/routes.js
@@ -4,15 +4,16 @@ var express = require('express');
 var api = express.Router();
 
 // API is only avaiable for authenticated users.
-/*
 api.use(function(req, res, next) {
 	if (req.isAuthenticated()) {
 		return next();
 	}
 
+	console.log(req.path);
+
 	res.json(401, {error: 'Not authenticated'});
 });
-*/
+
 
 api.use('/modules', require('../resources/Modules'));
 api.use('/subjects', require('../resources/Subjects'));
