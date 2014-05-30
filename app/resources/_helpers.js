ecc634005072f4270fba63530cbe13476996d95a
Christoph Jerolimov <jerolimov+git+2014@gmail.com>
2014-05-30 03:11:05 +0200

    Log mongoose errors to console.

--- app/resources/_helpers.js
+++ app/resources/_helpers.js
@@ -3,6 +3,7 @@ module.exports = {
 	sendResult: function(res) {
 		return function(err, response) {
 			if (err) {
+				console.error(err);
 				res.json(500, err);
 			} else {
 				res.json(200, response);
