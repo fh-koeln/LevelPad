1d7e39a5d20a4d391bbbbc236fcdd8b6a0e7efb6
BenWur <benwurth@web.de>
2014-07-14 17:07:42 +0200

    Tab Bars, Admin Stuff and active inactive Filter alpha

--- public/controllers/subjects/SubjectListController.js
+++ public/controllers/subjects/SubjectListController.js
@@ -1,12 +1,20 @@
 /* global angular, alert */
 
 angular.module('levelPad').controller('SubjectListController', [
-	'$scope', '$routeParams', '$location', '$log', 'Module', 'Subject', 'CurrentModule', 'CurrentSubject',
-	function ($scope, $routeParams, $location, $log, Module, Subject, CurrentModule, CurrentSubject) {
+	'$scope', '$http', '$routeParams', '$location', '$log', 'Module', 'Subject', 'CurrentModule', 'CurrentSubject',
+	function ($scope, $http, $routeParams, $location, $log, Module, Subject, CurrentModule, CurrentSubject) {
 	'use strict';
 	$scope.module = CurrentModule;
 	$scope.subject = CurrentSubject;
 
+    $scope.update = function () {
+		// Get all subjects for the current module
+		$http.get('/api/subjects').success(function(subjects) {
+		$scope.subjects = subjects;
+	});
+	};
+    $scope.update();
+        
 	$scope.go = function(path) {
 		$location.path(path);
 	};
