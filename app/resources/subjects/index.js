96fdedb99e351963448fc63376e5ea3e71ce3d78
dvizzacc <dariovizz@hotmail.it>
2014-07-14 13:56:49 +0200

    changed /Subjects.js to proper path /subjects/index.js

---
+++ app/resources/subjects/index.js
@@ -0,0 +1,61 @@
+var express = require('express'),
+	subjects = express.Router(),
+	Subject = require('../../models/Subject'),
+	Module = require('../../models/Module'),
+	helpers = require('../_helpers');
+require('url');
+
+
+//subjects.use('/:slug/artifacts', require('./Artifacts'));
+
+/**
+ * Get all subjects
+ */
+subjects.get('/', function (req, res) {
+	if (req.query.year || req.query.semester || req.query.module) {
+		
+		//convert Semester shortname to longname
+		if (req.query.semester === 'sose')
+			req.query.semester = 'Sommersemester';
+		else if (req.query.semester === 'wise')
+			req.query.semester = 'Wintersemester';
+		else
+			delete req.query.semester;
+
+		//Select ID from module-collection by shortname
+		if (req.query.module) {
+			Module.findBySlug(req.query.module, function (err, module) {
+				if (err) {
+					console.error(err);
+					res.json(500, err);
+				} 
+				
+				else if (module) {
+					console.log(module);
+					req.query.module = module._id;
+
+				}
+				findSubjects(req, res);
+			});
+		}
+		else
+			findSubjects(req,res);
+
+	} else {
+		Subject.find().populate('module').exec(helpers.sendResult(res));
+	}
+});
+
+//Helper-Function
+function findSubjects(req, res) {
+	Subject.find(
+		req.query
+	).populate({
+		path: 'module',
+	}).exec(helpers.sendResult(res));
+
+}
+
+
+
+module.exports = subjects;
