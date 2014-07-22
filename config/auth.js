91d293ecc4cbd0094b646ac38d126ea28dd7b0ea
Christoph Jerolimov <jerolimov+git+2014@gmail.com>
2014-07-22 18:22:39 +0200

    Extract fh-koeln auth strategy into own class.

--- config/auth.js
+++ config/auth.js
@@ -1,79 +1,10 @@
 'use strict';
 
-var express = require('express'),
-	app = express(),
-	passport = require('passport'),
+var passport = require('passport'),
 	LocalStrategy = require('passport-local').Strategy,
-	Imap = require('imap'),
-	User = require('../app/models/User'),
-	acl = require('./acl').acl;
-
-var checkCredentials = function(username, password, callback) {
-	// In development don't use the IMAP process
-	if (app.get('env') === 'development') {
-		console.log('Search user ' + username + ' in database..');
-		User.findByUsername(username, function(err, user) {
-			if (err) {
-				callback(null, false, err);
-			} else if (!user) {
-				// Create guest user which has to sign up
-				acl.addUserRoles(username, 'guest', function() {
-					user = new User();
-					user.username = username;
-					callback(null, user);
-				});
-			} else {
-				callback(null, user);
-			}
-		});
-		return;
-	}
-
-	console.log('Check IMAP credentials for user ' + username + '...');
-
-	// Asynchronous verification, for effect...
-	process.nextTick(function () {
-		// Login via IMAP
-		var imap = new Imap({
-			user: username,
-			password: password,
-			host: 'imap.intranet.fh-koeln.de',
-			port: 993,
-			keepalive: false,
-			tls: true
-		});
-
-		// Connect to IMAP-Server
-		imap.connect();
-
-		// Promise-Handler if login successful
-		imap.once('ready', function() {
-			imap.end();
-
-			User.findByUsername(username, function(err, user) {
-				if (err) {
-					callback(null, false, err);
-				} else if (!user) {
-					// Create guest user which has to sign up
-					acl.addUserRoles(username, 'guest', function() {
-						user = new User();
-						user.username = username;
-						callback(null, user);
-					});
-				} else {
-					callback(null, user);
-				}
-			});
-		});
-
-		// Promise-Handler if login failed
-		//imap.once('error', callback);
-		imap.once('error', function(err) {
-			console.log(err);
-			callback(err);
-		});
-	});
-};
+	fhKoelnImap = require('./strategies/fh-koeln-imap'),
+	acl = require('./acl').acl,
+	User = require('../app/models/User');
 
 /**
  * Passport session setup.
@@ -112,28 +43,7 @@ passport.deserializeUser(function(username, done) {
  * with a user object.  In the real world, this would query a database;
  * however, in this example we are using a baked-in set of users.
  */
-passport.use('fh-imap', new LocalStrategy(function(username, password, done) {
-	checkCredentials(username, password, function(err) {
-		if (err) {
-			done(null, false, err);
-		} else {
-			User.findByUsername(username, function(err, user) {
-				if (err) {
-					done(null, false, err);
-				} else if (!user) {
-					// Create guest user which has to sign up
-					acl.addUserRoles(username, 'guest', function() {
-						user = new User();
-						user.username = username;
-						done(null, user);
-					});
-				} else {
-					done(null, user);
-				}
-			});
-		}
-	});
-}));
+passport.use('fh-imap', new LocalStrategy(fhKoelnImap));
 
 module.exports = function(app) {
 
---
+++ config/strategies/fh-koeln-imap.js
@@ -0,0 +1,94 @@
+
+var Imap = require('imap'),
+	User = require('../../app/models/User'),
+	acl = require('../acl').acl;
+
+var checkCredentials = function(username, password, callback) {
+	// In development don't use the IMAP process
+	if (process.env.NODE_ENV === 'development') {
+		console.log('Search user ' + username + ' in database..');
+		User.findByUsername(username, function(err, user) {
+			if (err) {
+				callback(null, false, err);
+			} else if (!user) {
+				// Create guest user which has to sign up
+				acl.addUserRoles(username, 'guest', function() {
+					user = new User();
+					user.username = username;
+					callback(null, user);
+				});
+			} else {
+				callback(null, user);
+			}
+		});
+		return;
+	}
+
+	console.log('Check IMAP credentials for user ' + username + '...');
+
+	// Asynchronous verification, for effect...
+	process.nextTick(function () {
+		// Login via IMAP
+		var imap = new Imap({
+			user: username,
+			password: password,
+			host: 'imap.intranet.fh-koeln.de',
+			port: 993,
+			keepalive: false,
+			tls: true
+		});
+
+		// Connect to IMAP-Server
+		imap.connect();
+
+		// Promise-Handler if login successful
+		imap.once('ready', function() {
+			imap.end();
+
+			User.findByUsername(username, function(err, user) {
+				if (err) {
+					callback(null, false, err);
+				} else if (!user) {
+					// Create guest user which has to sign up
+					acl.addUserRoles(username, 'guest', function() {
+						user = new User();
+						user.username = username;
+						callback(null, user);
+					});
+				} else {
+					callback(null, user);
+				}
+			});
+		});
+
+		// Promise-Handler if login failed
+		//imap.once('error', callback);
+		imap.once('error', function(err) {
+			console.log(err);
+			callback(err);
+		});
+	});
+};
+
+module.exports = function(username, password, done) {
+	checkCredentials(username, password, function(err) {
+		if (err) {
+			done(null, false, err);
+		} else {
+			User.findByUsername(username, function(err, user) {
+				if (err) {
+					done(null, false, err);
+				} else if (!user) {
+					// Create guest user which has to sign up
+					acl.addUserRoles(username, 'guest', function() {
+						user = new User();
+						user.username = username;
+						done(null, user);
+					});
+				} else {
+					done(null, user);
+				}
+			});
+		}
+	});
+};
