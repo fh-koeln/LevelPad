1afc22195a421f93d33c97e6eecf6cd52b1e29f1
dvizzacc <dariovizz@hotmail.it>
2014-10-06 17:02:56 +0200

    Validation improvements

--- app/models/User.js
+++ app/models/User.js
@@ -17,27 +17,56 @@ function validateUsernameLength(username) {
 	}
 }
 
-function validateStudentNumber(studentNumber) {
-  	if ( ! /^\d+$/.test(studentNumber) ) {
-    	return false;
+function validateNamesLength(name) {
+	if (name.length < 2) {
+		return false;
 	} else {
+		return true;
+	}
+}
+
+function validateStudentNumber(studentNumber) {
+  	if ( /^[0-9]{8}$/.test(studentNumber) ) {
     	return true;
+	} else {
+    	return false;
+    }
+}
+
+function validateEmail(email) {
+  	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if ( re.test(email) ) {
+    	return true;
+	} else {
+    	return false;
     }
 }
 
 var userNameValidations = [
-	{ validator: validateUsernameLength, msg: 'Der Benutzername ist ungültig' }
+	{ validator: validateUsernameLength, msg: 'Der Benutzername ist ungültig.' }
+];
+
+var firstnameValidations = [
+	{ validator: validateNamesLength, msg: 'Der Vorname ist ungültig.' }
+];
+
+var lastnameValidations = [
+	{ validator: validateNamesLength, msg: 'Der Nachname ist ungültig.' }
 ];
 
 var studentNumberValidations = [
-	{ validator: validateStudentNumber, msg: 'Die Matrikelnummer ist ungültig' }
+	{ validator: validateStudentNumber, msg: 'Die Matrikelnummer ist ungültig.' }
+];
+
+var emailValidations = [
+	{ validator: validateEmail, msg: 'Die E-Mail-Adresse ist ungültig.' }
 ];
 
 var userSchema = new Schema({
 	username: { type: String, required: true, lowercase: true, unique: true, trim: true, validate: userNameValidations },
-	firstname: { type: String, required: true },
-	lastname: { type: String, required: true },
-	email: { type: String, required: true },
+	firstname: { type: String, required: true, validate: firstnameValidations },
+	lastname: { type: String, required: true, validate: lastnameValidations },
+	email: { type: String, required: true, validate: emailValidations },
 	studentNumber: { type: String, required: false, validate: studentNumberValidations },
 	role: { type: String, required: true, enum: [ 'guest', 'student', 'lecturer', 'assistent', 'administrator' ] }
 });
