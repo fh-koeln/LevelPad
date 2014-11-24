311e3eb7a084f298f8a8ae602306c80bd5bdf0d3
Dominik Schilling <dominikschilling+git@gmail.com>
2014-11-24 12:51:52 +0100

    Add default expire dates

--- public/controllers/DashboardController.js
+++ public/controllers/DashboardController.js
@@ -41,11 +41,43 @@ angular.module('levelPad').controller('DashboardController', [
 			$scope.semesters = semesters;
 		});
 
+		function zeroize(x) {
+			return (x < 10) ? '0' + x : x;
+		}
+
+		function getExpireDates() {
+			var dates = [],
+				today, actualDate, todayInTwoWeeks;
+
+			actualDate = new Date(),
+			today = new Date(actualDate.getFullYear(), actualDate.getMonth() ,actualDate.getDate(), 23, 59, 59);
+			todayInTwoWeeks = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 14);
+
+			dates.push({
+				name: 'Unbeschränkt',
+				timestamp: -1
+			});
+
+			while (today <= todayInTwoWeeks) {
+				dates.push({
+					name: zeroize(today.getDate()) + '.' +  zeroize(today.getMonth() + 1) + '.' + today.getFullYear(),
+					timestamp: today.getTime()
+				});
+				today.setDate(today.getDate() + 1);
+			}
+
+			return dates;
+		}
+
 		$scope.showCreateDialog = function() {
 			var dialog = new DialogService('/subjects/new');
 			dialog.scope.subject = new Subject();
 			dialog.scope.years = $scope.years;
 			dialog.scope.semesters = $scope.semesters;
+			dialog.scope.expireDates = getExpireDates();
+			dialog.scope.subject.registrationExpiresAt = dialog.scope.expireDates[0].timestamp;
+			dialog.scope.subject.registrationActive = 1;
+			console.log(dialog.scope.subject);
 			dialog.scope.submit = function() {
 				dialog.scope.module.$save(function() {
 					dialog.submit();
