8cc072a2f5253d6c80d6b73a426ebf5cfe93970a
Dominik Schilling <dominikschilling+git@gmail.com>
2014-11-24 16:34:00 +0100

    Update subjects list

--- public/controllers/subjects/SubjectListController.js
+++ public/controllers/subjects/SubjectListController.js
@@ -3,40 +3,47 @@
 angular.module('levelPad').controller('SubjectListController', [
 	'$scope', '$http', '$routeParams', '$location', '$log', 'Module', 'Subject', 'DialogService', 'CurrentModule', 'CurrentSubject',
 	function ($scope, $http, $routeParams, $location, $log, Module, Subject, DialogService, CurrentModule, CurrentSubject) {
-	'use strict';
-	$scope.module = CurrentModule;
-	$scope.subject = CurrentSubject;
 
-    $scope.update = function () {
+	'use strict';
+	$scope.module = $scope.module || CurrentModule || new Module();
+	$scope.subject = $scope.subject || CurrentSubject || new Subject();
+
+
+	$scope.update = function () {
+		$scope.subjects = [];
 		// Get all subjects for the current module
-		$http.get('/api/subjects').success(function(subjects) {
-		$scope.subjects = subjects;
-	});
+		Module.query(function(modules) {
+			angular.forEach(modules, function(module) {
+				Subject.query({ module: module.slug}, function(subjects) {
+					angular.forEach(subjects, function(subject) {
+						$scope.subjects.push(subject);
+						console.log($scope.subjects);
+					});
+				});
+			});
+		}, function() {
+			alert('Could not load subjects.');
+		});
 	};
-    $scope.update();
-        
+	$scope.update();
+
 	$scope.go = function(path) {
 		$location.path(path);
 	};
 
-	$scope.update = function() {
-
+	$scope.showCreateDialog = function() {
+		var dialog = new DialogService('/subjects/new');
+		dialog.scope.subject = new Subject();
+		dialog.scope.submit = function() {
+			dialog.scope.module.$save(function() {
+				dialog.submit();
+				$scope.update();
+			}, function() {
+				alert('Fehler!');
+			});
+		};
+		dialog.open();
 	};
-	$scope.update();
-
-    $scope.showCreateDialog = function() {
-        var dialog = new DialogService('/subjects/new');
-        dialog.scope.subject = new Subject();
-        dialog.scope.submit = function() {
-            dialog.scope.module.$save(function() {
-                dialog.submit();
-                $scope.update();
-            }, function() {
-                alert('Fehler!');
-            });
-        };
-        dialog.open();
-    };
 
 	$scope.showEditDialog = function(subject) {
 		$scope.subject = angular.copy(subject);
@@ -64,6 +71,6 @@ angular.module('levelPad').controller('SubjectListController', [
 		}, function() {
 			alert('Error!');
 		});
-	};    
-        
+	};
+
 }]);
