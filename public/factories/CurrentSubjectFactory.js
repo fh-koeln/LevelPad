eb1da688d447bc936ad7149c2531b4368a5d509b
Christoph Jerolimov <jerolimov+git+2014@gmail.com>
2014-12-01 16:57:33 +0100

    Work on different list views

--- public/factories/CurrentSubjectFactory.js
+++ public/factories/CurrentSubjectFactory.js
@@ -1,7 +1,13 @@
 
-angular.module('levelPad').factory('CurrentSubject', ['$routeParams', 'Subject', function($routeParams, Subject) {
-	return Subject.get({
-		module: $routeParams.module,
-		subject: $routeParams.subject
-	});
+angular.module('levelPad').factory('CurrentSubject', ['$routeParams', '$q', 'Subject', function($routeParams, $q, Subject) {
+	if (!$routeParams.module) {
+		return $q.reject('Route parameter module is not defined to get current subject.');
+	} else if ($routeParams.subject) {
+		return $q.reject('Route parameter subject is not defined to get current subject.');
+	} else {
+		return Subject.get({
+			module: $routeParams.module,
+			subject: $routeParams.subject
+		});
+	}
 }]);
--- public/factories/CurrentTaskFactory.js
+++ public/factories/CurrentTaskFactory.js
@@ -1,8 +1,16 @@
 
-angular.module('levelPad').factory('CurrentTask', ['$routeParams', 'Task', function($routeParams, Task) {
-	return Task.get({
-		module: $routeParams.module,
-		subject: $routeParams.subject,
-		task: $routeParams.task
-	});
+angular.module('levelPad').factory('CurrentTask', ['$routeParams', '$q', 'Task', function($routeParams, $q, Task) {
+	if (!$routeParams.module) {
+		return $q.reject('Route parameter module is not defined to get current task.');
+	} else if ($routeParams.subject) {
+		return $q.reject('Route parameter subject is not defined to get current task.');
+	} else if ($routeParams.task) {
+		return $q.reject('Route parameter task is not defined to get current task.');
+	} else {
+		return Task.get({
+			module: $routeParams.module,
+			subject: $routeParams.subject,
+			task: $routeParams.task
+		});
+	}
 }]);
