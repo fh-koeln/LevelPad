cac099e2756aca2456c065c94f35e9bc66a6f08d
BenWur <benwurth@web.de>
2014-12-10 10:50:09 +0100

    Member view

--- public/controllers/members/MemberDetailController.js
+++ public/controllers/members/MemberDetailController.js
@@ -7,23 +7,32 @@ angular.module('levelPad').controller('MemberDetailController', [
 		'use strict';
 		$scope.module = CurrentModule;
 		$scope.subject = CurrentSubject;
-		$scope.member = CurrentMember;
-
-		console.log('member:', $scope.member);
+		var relGrade = 0;
+		var weightSum = 0;
+		
+		
 
 		$scope.go = function(path) {
 			$location.path(path);
 		};
 
 		$scope.update = function() {
-			$scope.modules = Module.query(function() {
-
-			}, function() {
-				alert('Could not load modules.');
-			});
+			if ($routeParams.member) {
+				$scope.member = Member.get({
+					module: $routeParams.module,
+					subject: $routeParams.subject,
+					member: $routeParams.member
+				}, function () {
+					prepareMember();
+				}
+			)}
 		};
 
 		$scope.update();
+		
+		console.log('member:', $scope.member);
+		console.log('Current module', CurrentModule);
+		console.log('Current subject', CurrentSubject);
 
 		$scope.showCreateDialog = function() {
 			var dialog = new DialogService('/tasks/new');
@@ -41,6 +50,7 @@ angular.module('levelPad').controller('MemberDetailController', [
 
 		//Pie Chart Magic
 		$scope.options = ChartOption;
+		
 		$scope.task ={
 			amountRange: 50
 		};
@@ -67,4 +77,65 @@ angular.module('levelPad').controller('MemberDetailController', [
 				];
 			}
 		);
+		
+		//function relativeGrade(){
+		//	angular.forEach($scope.subject.tasks, function(task) {
+		//		weightSum += task.weight;
+		//	}
+			
+		//	return relGrade
+		//};
+		
+		function prepareMember() {
+			console.log($scope.member);
+			console.log($scope.subject);
+			
+			
+			$scope.member._artefacts = [
+				{
+					title:'Artefakte',
+					value: $scope.member.evaluations.length,
+					color: '#77cc00',
+					highlight: '#88dd11'
+				},
+				{
+					title:'Rest',
+					value: $scope.subject.tasks.length - $scope.member.evaluations.length,
+					color:'lightgray',
+					highlight: 'lightgray'
+				}
+			];
+			$scope.member.relGrade = [
+				{
+					title:'Note',
+					value: 2,
+					color: '#77cc00',
+					highlight: '#88dd11'
+				},
+				{
+					title:'Rest',
+					value: 5 - 2,
+					color:'lightgray',
+					highlight: 'lightgray'
+				}
+			];
+			
+			angular.forEach($scope.subject.tasks, function(task) {
+				task._taskWeight = [
+				{
+					title:'Task',
+					value: task.weight,
+					color: '#77cc00',
+					highlight: '#88dd11'
+				},
+				{
+					title:'Rest',
+					value: 100- task.weight,
+					color:'lightgray',
+					highlight: 'lightgray'
+				}
+			];
+			});
+		}
+
 	}]);
